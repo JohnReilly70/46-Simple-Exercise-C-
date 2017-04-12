664a37bebb86c86fdfc6f94bebecfc5bd4cb709d
SgtSafety <johnreilly1989@hotmail.co.uk>
2017-04-12 14:37:24 +0100

    Created template class to handle any type of numbers being compared

--- Challenge 1/Challenge 1/Challenge 1.cpp
+++ Challenge 1/Challenge 1/Challenge 1.cpp
@@ -7,7 +7,18 @@
 using namespace std;
 
 
-float max(float value1, float value2) {
+//float max(float value1, float value2) {
+//	if (value1 > value2) {
+//		return value1;
+//	}
+//	else {
+//		return value2;
+//	}
+//}
+
+template <class T, class U>
+T max(T value1, U value2)
+{
 	if (value1 > value2) {
 		return value1;
 	}
@@ -16,12 +27,18 @@ float max(float value1, float value2) {
 	}
 }
 
+
+
 int main()
 {
 	float max_value;
 	max_value = max(1, 2);
 	cout << max_value << endl;
 	max_value = max(10, 5);
+	cout << max_value << endl;
+	max_value = max<int>(5, 5.99);
+	cout << max_value << endl;
+	max_value = max<float>(5.99, 5);
 	cout << max_value << endl;
 	return 0;
 }
